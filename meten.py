779c85f75adaddec68196c020dc62d29fedc17fc
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-01-26 13:11:29 +0100

    input en if-statement

---
+++ meten.py
@@ -0,0 +1,9 @@
+print("Vul een geheel nummer in.")
+numA = int(input())
+print("Vul een ander geheel nummer in.")
+numB = int(input())
+Max = 0
+
+if(numA > numB):
+    Max = numA
+    print("a is het grootste getal: "+str(Max))
