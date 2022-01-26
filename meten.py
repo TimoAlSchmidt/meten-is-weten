8301f2bb13f938655fd65fdd3c16fb76f38edf65
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-01-26 13:14:25 +0100

    Min en Max

--- meten.py
+++ meten.py
@@ -3,12 +3,20 @@ numA = int(input())
 print("Vul een ander geheel nummer in.")
 numB = int(input())
 Max = 0
+Min = 0
 
 if(numA > numB):
     Max = numA
+    Min = numB
     print("a is het grootste getal: "+str(Max))
 elif(numA < numB):
     Min = numA
+    Max = numB
     print("a is het kleinste getal: "+str(Min))
 else:
+    Min = numA
+    Max = Min
     print("a en b zijn even groot")
+
+print("Het minimum is: "+str(Min))
+print("Het maximum is: "+str(Max))
